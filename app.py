f06a6bb9df33416b822be539ff4ace4e5b7056dd
gians <gian.villca@sistemas.edu.bo>
2025-12-05 16:36:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
-# app.py (versión vulnerable)
-from flask import Flask, request, render_template_string
+# app.py
+from flask import Flask, request
 import auth
 
 app = Flask(__name__)
 
-LOGIN_PAGE = """
-<h2>Login vulnerable (educativo)</h2>
-<form method=post>
-  Usuario: <input name="username"><br>
-  Contraseña: <input name="password" type="password"><br>
-  <input type=submit value="Entrar">
-</form>
-<p>{{ message }}</p>
-"""
-
 @app.route("/", methods=["GET", "POST"])
 def login():
-    message = ""
     if request.method == "POST":
         password = request.form.get("password", "")
         ok, msg = auth.check_password_vulnerable(password)
-        message = msg
         if ok:
-            return "<h2>Login exitoso (vulnerable)</h2>"
-    return LOGIN_PAGE.replace("{{ message }}", message)
+            return "LOGIN OK"
+        else:
+            return "FAIL"
+    
+    return """
+    <h2>Login vulnerable</h2>
+    <form method="post">
+        Contraseña: <input type="password" name="password"><br>
+        <input type="submit" value="Entrar">
+    </form>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
